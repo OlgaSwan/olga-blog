c907a053235a8872a536b8759d0a0f06cacf4931
unordinarity <unordinarity@yandex.ru>
2023-06-23 15:26:59 +0300

    feat: mobile navigation menu

--- src/shared/template/header.tsx
+++ src/shared/template/header.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent, useContext } from 'react'
-import { Avatar, Box, Header, Nav, ResponsiveContext } from 'grommet'
+import React, { FunctionComponent, useContext, useRef, useState } from 'react'
+import { Avatar, Box, Drop, Header, Heading, Layer, Nav, ResponsiveContext } from 'grommet'
 import * as Icons from 'grommet-icons'
 import { useStore } from '@nanostores/react'
 
@@ -12,29 +12,70 @@ import avatar from './avatar.jpg'
 
 const TemplateHeaderMobile: FunctionComponent = () => {
   const colorSchemeValue = useStore(colorScheme)
+  const authStoreValue = useStore(auth.store)
+  const [isMenuOpened, setMenuOpened] = useState<boolean>(false)
 
   return (
-    <Header
-      sticky='scrollup'
-      height='xsmall'
-      pad='medium'
-      background={{ color: 'background', opacity: 'weak' }}
-      style={{ backdropFilter: 'blur(6px)' }}
-    >
-      <Box direction='row' gap='small' align='center'>
+    <>
+      <Header
+        sticky='scrollup'
+        height='xsmall'
+        pad='medium'
+        background={{ color: 'background', opacity: 'weak' }}
+        style={{ backdropFilter: 'blur(6px)' }}
+      >
         <LinkCustom size='large' label={<Avatar src={avatar} size='medium' />} href={routeMap.home.path} />
         <LinkCustom size='large' label='Olga Swan' href={routeMap.home.path} />
-      </Box>
-      <LinkCustom
-        icon={<Icons.Menu />}
-        href='#'
-        onClick={(event) => {
-          event.preventDefault()
-          colorScheme.set(colorSchemeValue === 'dark' ? 'light' : 'dark')
-        }}
-        style={{ lineHeight: 0 }}
-      />
-    </Header>
+        <LinkCustom
+          icon={<Icons.Menu />}
+          href='#'
+          onClick={(event) => {
+            event.preventDefault()
+            setMenuOpened(!isMenuOpened)
+          }}
+          style={{ lineHeight: 0 }}
+        />
+      </Header>
+      {isMenuOpened && (
+        <Layer
+          modal
+          position='top-right'
+          responsive={false}
+          margin='small'
+          onClickOutside={() => setMenuOpened(false)}
+          onEsc={() => setMenuOpened(false)}
+        >
+          <Nav pad='large' gap='large'>
+            <LinkCustom size='xlarge' label='Blog' href={routeMap.blogHome.path} />
+            <LinkCustom size='xlarge' label='About' href={routeMap.aboutMe.path} />
+            <LinkCustom size='xlarge' label='Uses' href={routeMap.aboutUses.path} />
+            <LinkCustom size='xlarge' label='Hire me' href={routeMap.aboutHire.path} />
+            {authStoreValue ? (
+              <LinkCustom
+                size='xlarge'
+                label='Sign out'
+                href='#'
+                onClick={(event) => {
+                  event.preventDefault()
+                  auth.logout()
+                }}
+              />
+            ) : (
+              <LinkCustom size='xlarge' label='Sign in' href={routeMap.authLogin.path} />
+            )}
+            <LinkCustom
+              label='Color scheme'
+              size='xlarge'
+              href='#'
+              onClick={(event) => {
+                event.preventDefault()
+                colorScheme.set(colorSchemeValue === 'dark' ? 'light' : 'dark')
+              }}
+            />
+          </Nav>
+        </Layer>
+      )}
+    </>
   )
 }
 
