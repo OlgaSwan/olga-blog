81623678cbc61563e9f09fba94ce165b4293f7e8
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-06-24 07:16:06 +0300

    blurp

--- src/shared/template/footer.tsx
+++ src/shared/template/footer.tsx
@@ -6,11 +6,11 @@ import { LinkCustom } from '../link-custom'
 
 export const TemplateFooter: FunctionComponent = () => (
   <Footer pad='medium'>
-    <Text>Copyright</Text>
+    <Text>© Olga Swan, 2023</Text>
     <Box direction='row' gap='small' justify='center'>
       <LinkCustom target='_blank' href='https://www.instagram.com/olyasswan/' icon={<Icons.Instagram />} />
+      <LinkCustom target='_blank' href='https://github.com/OlgaSwan' icon={<Icons.Github />} />
       <LinkCustom target='_blank' href='https://www.behance.net/olyasswan' icon={<Icons.Paint />} />
-      <LinkCustom target='_blank' href='https://rus.bongacams.com/' icon={<Icons.Webcam />} />
     </Box>
   </Footer>
 )
