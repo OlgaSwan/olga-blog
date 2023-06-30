7e6da4ceb66813845614896e5ae9fdecca9e22a1
unordinarity <unordinarity@yandex.ru>
2023-06-30 07:25:41 +0300

    refactor: auth entity refinement

--- src/model/auth.ts
+++ src/model/auth.ts
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut, User } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut, User, onAuthStateChanged } from 'firebase/auth'
 import { atom } from 'nanostores'
 
 import { firebaseApp } from '../shared/firebase-app'
@@ -7,41 +7,17 @@ const firebaseAuth = getAuth(firebaseApp)
 
 const store = atom<User | null>(firebaseAuth.currentUser)
 
-const login = (loginData: { login: string; password: string }) => {
-  signInWithEmailAndPassword(firebaseAuth, loginData.login, loginData.password)
-    .then((userCredential) => {
-      store.set(userCredential.user)
-    })
-    .catch((error) => {
-      store.set(null)
-      console.error(error)
-    })
-}
-
-const logout = () => {
-  signOut(firebaseAuth)
-    .then(() => {
-      store.set(null)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-}
-
-const register = (loginData: { login: string; password: string }) => {
-  createUserWithEmailAndPassword(firebaseAuth, loginData.login, loginData.password)
-    .then((userCredential) => {
-      store.set(userCredential.user)
-    })
-    .catch((error) => {
-      store.set(null)
-      console.error(error)
-    })
-}
+onAuthStateChanged(firebaseAuth, user => store.set(user))
 
 export const auth = {
   store,
-  login,
-  logout,
-  register,
+  login: async (loginData: { login: string; password: string }) => {
+    await signInWithEmailAndPassword(firebaseAuth, loginData.login, loginData.password)
+  },
+  logout: async () => {
+    await signOut(firebaseAuth)
+  },
+  register: async (loginData: { login: string; password: string }) => {
+    await createUserWithEmailAndPassword(firebaseAuth, loginData.login, loginData.password)
+  }
 }
