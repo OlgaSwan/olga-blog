eb91eb7d20b0f053e9f6f2dba78c75c77561e86f
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-07-06 22:45:14 +0300

    fix: redirect

--- src/pages/blog-diary-id/page.tsx
+++ src/pages/blog-diary-id/page.tsx
@@ -1,52 +1,43 @@
-import React, { FunctionComponent, PropsWithChildren, useContext, useEffect, useState } from 'react'
-import { useParams, useNavigate, redirect } from 'react-router-dom'
+import React, { FunctionComponent, useContext } from 'react'
+import { useParams, Navigate } from 'react-router-dom'
 import { useStore } from '@nanostores/react'
 import { Box, Button, Card, CardBody, CardFooter, CardHeader, Tag, Text, PageHeader, ResponsiveContext } from 'grommet'
 import * as Icons from 'grommet-icons'
 
 import { Template } from '../../shared/template'
 import { diaryStore } from '../../model/diary'
-import { DiaryExternal } from '../../model/diary'
 import { routeMap } from '..'
 
 const BlogDiaryId: FunctionComponent = () => {
   const allDiaries = useStore(diaryStore.list)
-  const [Diary, setDiary] = useState<DiaryExternal>()
   const params = useParams()
-  const navigate = useNavigate()
+  const foundDiary = allDiaries.find((p) => p.id === params.id) 
   const screenSize = useContext(ResponsiveContext)
-  useEffect(() => {
-    const foundDiary = allDiaries.find((p) => p.id === params.id)
-    if (!foundDiary) {
-      navigate(routeMap.errorNotFound.path)
-    }
-    setDiary(foundDiary)
-  }, [])
 
   return (
     <Template>
-      {Diary && (
+      {foundDiary ? (
         <>
           <PageHeader size='small' title='Diary' margin={{ bottom: 'large', top: 'large' }} />
           <Card width='large' background='light-1'>
             <CardHeader direction='column' pad='medium' focusIndicator={false}>
               <Text size='3xl' weight='bold' alignSelf='start'>
-                {Diary!.title}
+                {foundDiary!.title}
               </Text>
             </CardHeader>
             <CardBody pad='medium' gap='medium' focusIndicator={false}>
               <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
-                {Diary!.content}
+                {foundDiary!.content}
               </Text>
               <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
-                {Diary!.content} {Diary!.content}
+                {foundDiary!.content} {foundDiary!.content}
               </Text>
               <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
-                {Diary!.content} {Diary!.content}
+                {foundDiary!.content} {foundDiary!.content}
               </Text>
               {screenSize !== 'small' && (
                 <Box direction='row' flex='grow' gap='small' alignSelf='start'>
-                  {Diary!.tags.map((tag) => (
+                  {foundDiary!.tags.map((tag) => (
                     <Tag key={tag} size='small' value={tag} />
                   ))}
                 </Box>
@@ -56,13 +47,15 @@ const BlogDiaryId: FunctionComponent = () => {
               <Box direction='row' align='center'>
                 <Button icon={<Icons.Favorite color='red' />} hoverIndicator />
                 <Text size='small' weight='normal'>
-                  {Diary!.likes}
+                  {foundDiary!.likes}
                 </Text>
               </Box>
               <Button icon={<Icons.ShareOption color='plain' />} hoverIndicator />
             </CardFooter>
           </Card>
         </>
+      ) : (
+        <Navigate to={routeMap.errorNotFound.path} />
       )}
     </Template>
   )
