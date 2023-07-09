cd4145c864f270f5160dec872d3c55d7516b5a6f
unordinarity <unordinarity@yandex.ru>
2023-07-09 06:53:20 +0300

    improve: skillset at home page

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react'
-import { PageHeader, Paragraph, Button, Box, Text } from 'grommet'
+import { PageHeader, Paragraph, Button, Heading } from 'grommet'
 import * as Icons from 'grommet-icons'
 
 import { routeMap } from '..'
@@ -10,20 +10,17 @@ import { DiaryList } from '../../model/diary'
 
 const Home: FunctionComponent = () => (
   <Template>
-    <PageHeader
-      size='medium'
-      title={`Howdy, I'm Olga!`}
-      subtitle={
-        <Box direction='row' gap='small' margin={{ top: 'medium' }}>
-          <Text size='large'>And welcome to my Blog</Text>
-          <Icons.Diamond size='medium' color='brand' />
-        </Box>
-      }
-      margin={{ top: 'medium' }}
-    />
-    <Paragraph style={{ maxWidth: '768px' }} margin={{ bottom: 'medium' }} size='large'>
+    <Heading level='1' margin={{ bottom: 'medium' }}>
+      Howdy, I'm Olga!
+    </Heading>
+    <Paragraph fill size='large' margin={{ bottom: 'medium' }}>
+      And welcome to my Blog
+      <Icons.Diamond size='medium' color='brand' />
+    </Paragraph>
+    <Paragraph fill size='large' margin={{ bottom: 'medium' }}>
       I'm a frontend developer from Moscow. I <LinkCustom label='write' href={routeMap.blogHome.path} /> about code,
-      ux/ui and art. <br /> Passionate about traditional and digital drawing, gaming and paleontology.
+      ux/ui and art. <br />
+      Passionate about traditional and digital drawing, gaming and paleontology.
     </Paragraph>
     <PageHeader size='small' title='Latest posts' margin={{ bottom: 'medium', top: 'small' }} />
     <DiaryList isSliced={true} />
@@ -35,14 +32,43 @@ const Home: FunctionComponent = () => (
       label='Show more'
       href={routeMap.blogHome.path}
     />
-    <PageHeader size='small' title='Technology stack' margin={{ bottom: 'small', top: 'small' }} />
-    <Paragraph style={{ maxWidth: '768px' }} margin={{ bottom: 'medium' }} size='large'>
+    <Heading level='2' margin={{ bottom: 'small' }}>
+      My skillset
+    </Heading>
+    <Heading level='3' margin={{ bottom: 'small' }}>
+      Hard-skills
+    </Heading>
+    <Paragraph margin={{ bottom: 'small' }}>
+      I've worked with a lot of technologies on petprojects and in production
       <ul>
-        <li>Lorem ipsum dolor sit amet.</li>
-        <li>
-          Est, corporis. <LinkCustom label='click here' href='#' />
-        </li>
-        <li>Pizda.</li>
+        <li>Vanilla JS, TypeScript</li>
+        <li>REST and CRUD APIs, Firebase</li>
+        <li>CSS processors and CSS-in-JS libraries: SASS, PostCSS, Stitches, Styled Components, etc</li>
+        <li>React ecosystem: UI kits and frameworks, SPA and SSR frameworks</li>
+        <li>State management libraries: Effector, RXjs, Redux</li>
+      </ul>
+    </Paragraph>
+    <Heading level='3' margin={{ bottom: 'small' }}>
+      T-shaped skills
+    </Heading>
+    <Paragraph margin={{ bottom: 'medium' }}>
+      I am mainly engaged in development, but I've learnt some similar areas
+      <ul>
+        <li>UI design</li>
+        <li>UX research and analytics</li>
+        <li>Design systems and UI kits</li>
+        <li>Product analytics</li>
+      </ul>
+    </Paragraph>
+    <Heading level='3' margin={{ bottom: 'small' }}>
+      Soft-skills
+    </Heading>
+    <Paragraph margin={{ bottom: 'medium' }}>
+      ALso, over time, I learned how to communicate with people and nuances of teamwork (maybe even better than some technologies)
+      <ul>
+        <li>Solution research and presentation</li>
+        <li>Management and delegation</li>
+        <li>Communication and conflict resolution</li>
       </ul>
     </Paragraph>
   </Template>
