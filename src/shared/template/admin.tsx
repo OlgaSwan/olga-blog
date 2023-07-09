c6e1f12da89dee9bce75d8fc3ec0d99f45161ddd
unordinarity <unordinarity@yandex.ru>
2023-07-09 15:02:46 +0300

    improve: admin pages template

--- src/shared/template/admin.tsx
+++ src/shared/template/admin.tsx
@@ -1,8 +1,24 @@
 import React, { FunctionComponent, PropsWithChildren } from 'react'
+import { Heading } from 'grommet'
+
 import { TemplateAbstract } from './abstract'
+import { LinkCustom } from '../link-custom'
+import { routeMap } from '../../pages'
+
+const Sidebar: FunctionComponent = () => (
+  <>
+    <Heading level='2'>Admin console</Heading>
+    <LinkCustom href={routeMap.adminHome.path} size='xlarge'>Home</LinkCustom>
+    <LinkCustom href={routeMap.adminDiaryList.path} size='xlarge'>Diaries</LinkCustom>
+    <LinkCustom href={routeMap.adminTagList.path} size='xlarge'>Tags</LinkCustom>
+  </>
+)
 
 export const TemplateAdmin: FunctionComponent<PropsWithChildren> = ({
   children,
 }) => (
-  <TemplateAbstract main={children} />
+  <TemplateAbstract
+    sidebarLeft={<Sidebar />}
+    main={children}
+  />
 )
