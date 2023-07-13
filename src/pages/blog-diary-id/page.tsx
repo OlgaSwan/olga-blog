f12c74b54f721aef7b373922bf5d71d00ab5958b
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-07-14 01:19:16 +0300

    minor fix

--- src/pages/blog-diary-id/page.tsx
+++ src/pages/blog-diary-id/page.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useContext } from 'react'
 import { useParams, useNavigate, Navigate } from 'react-router-dom'
 import { useStore } from '@nanostores/react'
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Tag, Text, ResponsiveContext, Heading } from 'grommet'
+import { Box, Button, Tag, Text, ResponsiveContext, Heading } from 'grommet'
 import * as Icons from 'grommet-icons'
 
 import { TemplateContent } from 'src/shared/template'
@@ -12,34 +12,18 @@ const BlogDiaryId: FunctionComponent = () => {
   const allDiaries = useStore(diaryStore.list)
   const allTags = useStore(diaryStore.tagList)
   const params = useParams()
-  const navigate = useNavigate()
   const foundDiary = allDiaries.find((p) => p.id === params.id)
+  const navigate = useNavigate()
   const screenSize = useContext(ResponsiveContext)
 
   return (
     <TemplateContent>
       {foundDiary ? (
         <>
-          <Text size='3xl' weight='bold' alignSelf='start' margin={{ top: 'xlarge', bottom: 'medium' }}>
+          <Heading size='medium' alignSelf='start' margin={{ top: 'xlarge', bottom: 'medium' }}>
             {foundDiary!.title}
-          </Text>
-          <Box gap='small' margin={{ top: 'small', bottom: 'small' }}>
-            <Box direction='row' flex='grow' gap='small' alignSelf='start'>
-              {foundDiary!.tags.map((tag) => (
-                <Tag
-                  key={tag}
-                  value={tag}
-                  size='small'
-                  // @ts-ignore
-                  style={{borderWidth: '2px'}}
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    const foundTag = allTags.find((t) => t.title === tag)
-                    navigate(`/blog/${foundTag!.id}`)
-                  }}
-                />
-              ))}
-            </Box>
+          </Heading>
+          <Box gap='medium' margin={{ bottom: 'medium' }}>
             <Box direction='row' align='center' alignSelf='start'>
               <Box direction='row' align='center' justify='between' gap='small'>
                 <Box direction='row' align='center'>
@@ -49,25 +33,51 @@ const BlogDiaryId: FunctionComponent = () => {
                 <Text size='small' weight='bold' style={{ lineHeight: '20px' }}>
                   ·
                 </Text>
-                <Text size='small'>{foundDiary.minRead + ' min read'}</Text>
+              </Box>
+              <Box direction='row' align='center' justify='between' gap='small'>
+                <Box direction='row' align='center'>
+                  <Button icon={<Icons.Clock color='text' />} hoverIndicator />
+                  <Text size='small'>{foundDiary.minRead + ' min read'}</Text>
+                </Box>
                 <Text size='small' weight='bold' style={{ lineHeight: '20px' }}>
                   ·
                 </Text>
               </Box>
-              <Button icon={<Icons.ShareRounded color='text' />} hoverIndicator />
+              <Box direction='row' align='center' justify='between' gap='small'>
+                <Box direction='row' align='center'>
+                  <Button icon={<Icons.ShareRounded color='text' />} hoverIndicator />
+                  <Text size='small'>{'Share'}</Text>
+                </Box>
+              </Box>
             </Box>
           </Box>
 
-          <Box margin={{ bottom: 'large' }}>
-          <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
-            {foundDiary!.content}
-          </Text>
-          <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
-            {foundDiary!.content}
-          </Text>
-          <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
-            {foundDiary!.content}
-          </Text>
+          <Box margin={{ bottom: 'medium' }}>
+            <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
+              {foundDiary!.content}
+            </Text>
+            <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
+              {foundDiary!.content}
+            </Text>
+            <Text size='medium' weight='normal' margin={{ bottom: 'medium' }}>
+              {foundDiary!.content}
+            </Text>
+          </Box>
+          <Box direction='row' flex='grow' gap='small' alignSelf='start' margin={{ bottom: 'large' }}>
+            {foundDiary!.tags.map((tag) => (
+              <Tag
+                key={tag}
+                value={tag}
+                size='small'
+                // @ts-ignore
+                style={{ borderWidth: '2px' }}
+                onClick={(e) => {
+                  e.stopPropagation()
+                  const foundTag = allTags.find((t) => t.title === tag)
+                  navigate(`/blog/${foundTag!.id}`)
+                }}
+              />
+            ))}
           </Box>
         </>
       ) : (
