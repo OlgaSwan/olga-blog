f7e3de8f5f30f82bd866bd3728927af9c83936ef
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-07-16 23:48:37 +0300

    ochen' mnogo plakala

--- src/model/diary/diary-list.tsx
+++ src/model/diary/diary-list.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useMemo } from 'react'
 import { useStore } from '@nanostores/react'
 import { Box } from 'grommet'
 
 import { diaryStore } from './store'
 import { DiaryCard } from './diary-card'
+
+import { filterDiaries } from 'src/shared/filterDiaries'
 
 export interface DiaryListProps {
   isSliced?: boolean
@@ -13,21 +15,18 @@ export interface DiaryListProps {
 export const DiaryList: FunctionComponent<DiaryListProps> = ({ isSliced = true, chosenTags = [] }) => {
   const allDiaries = useStore(diaryStore.list)
 
+  let allDiariesMemo = useMemo(
+    () => filterDiaries(allDiaries, isSliced, chosenTags),
+    [allDiaries, isSliced, chosenTags],
+  )
+
+  if (allDiariesMemo === null) return null
+
   return (
     <Box gap='medium'>
-      {chosenTags.length !== 0
-        ? allDiaries
-            .filter((diary) => {
-              for (let tag of diary.tags) {
-                let foundTag = chosenTags.includes(tag)
-                if (foundTag) return true
-              }
-              return false
-            })
-            .map((p) => <DiaryCard key={p.id} id={p.id} />)
-        : isSliced
-        ? allDiaries.slice(0, 3).map((p) => <DiaryCard key={p.id} id={p.id} />)
-        : allDiaries.map((p) => <DiaryCard key={p.id} id={p.id} />)}
+      {allDiariesMemo.map((d) => (
+        <DiaryCard key={d.id} id={d.id} />
+      ))}
     </Box>
   )
 }
