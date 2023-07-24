ae979bb0342753f6eb82a5c4c7143589fc035e4b
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-07-24 21:03:38 +0300

    razmazala no ewe ne rdy

--- src/pages/admin-diary-id/page.tsx
+++ src/pages/admin-diary-id/page.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from 'react'
-import { Heading } from 'grommet'
+import React, { FunctionComponent, useState } from 'react'
+import { Box, Button, Heading, TextInput, TextArea } from 'grommet'
+import * as Icons from 'grommet-icons'
 
 import { TemplateAdmin } from 'src/shared/template'
 import { useAuthRedirect } from 'src/model/auth'
@@ -7,12 +8,31 @@ import { routeMap } from '../index'
 
 const AdminDiaryId: FunctionComponent = () => {
   useAuthRedirect(true, routeMap.errorForbidden.path)
+  const [titleValue, setTitleValue] = useState('')
+  const [contentValue, setContentValue] = useState('')
 
   return (
     <TemplateAdmin>
       <Heading level='2' margin={{ bottom: 'medium' }}>
         Diary editor
       </Heading>
+      <Box gap='small' margin={{ top: 'medium' }}>
+        <Box>
+          <TextInput placeholder='Title' value={titleValue} onChange={(event) => setTitleValue(event.target.value)} />
+        </Box>
+        <Box>
+          <TextArea
+            placeholder='Content'
+            value={contentValue}
+            onChange={(event) => setContentValue(event.target.value)}
+          />
+        </Box>
+        <Box direction='row' gap='small' margin={{ top: 'small' }}>
+          <Heading level='4'>Add tags</Heading>
+          <Icons.AddCircle size='medium' color='placeholder' />
+        </Box>
+        <Button primary label='Create' size='small' margin={{ top: 'medium' }} style={{ width: '200px' }} />
+      </Box>
     </TemplateAdmin>
   )
 }
