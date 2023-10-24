1a06475dbcd645b2b84495116b27609e81fc928c
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-10-24 06:02:40 +0300

    implement share button

--- src/model/diary/diary-card.tsx
+++ src/model/diary/diary-card.tsx
@@ -1,6 +1,18 @@
 import React, { FunctionComponent, PropsWithChildren, useContext, useState } from 'react'
 import { createPath, createSearchParams, useNavigate } from 'react-router-dom'
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Heading, ResponsiveContext, Tag, Text } from 'grommet'
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  Notification,
+  ResponsiveContext,
+  Tag,
+  Text
+} from 'grommet'
 import * as Icons from 'grommet-icons'
 
 import { useStore } from '@nanostores/react'
@@ -9,6 +21,7 @@ import { diaryStore } from './store'
 import { routeMap } from 'src/shared/route-map'
 
 import { useReadTime } from 'src/shared/hooks/useReadTime'
+import { getUrl } from 'src/shared/utils/get-url'
 
 export interface DiaryCardProps {
   id: string
@@ -22,6 +35,8 @@ export const DiaryCard: FunctionComponent<PropsWithChildren<DiaryCardProps>> = (
     const likedIds = getLikedIds()
     return likedIds.includes(id)
   })
+  const [open, setOpen] = useState(false)
+
   const navigate = useNavigate()
   const screenSize = useContext(ResponsiveContext)
   const readTime = useReadTime(foundDiary?.content)
@@ -45,60 +60,73 @@ export const DiaryCard: FunctionComponent<PropsWithChildren<DiaryCardProps>> = (
   }
 
   return (
-    <Card width='large' background={{ color: 'light-1' }}>
-      <CardHeader direction='column' pad='medium' focusIndicator={false} onClick={() => navigate(`/blog/diary/${id}`)}>
-        {screenSize !== 'small' && (
-          <Box direction='row' flex='grow' gap='small' alignSelf='end'>
-            {foundDiary.tags.map((tag) => (
-              <Tag
-                key={tag}
-                value={tag}
-                size='small'
-                // @ts-ignore
-                style={{ borderWidth: '2px' }}
-                onClick={(e) => {
-                  e.stopPropagation()
-                  navigate(
-                    createPath({
-                      pathname: routeMap.blogHome,
-                      search: createSearchParams({ tags: [tag] }).toString()
-                    })
-                  )
-                }}
-              />
-            ))}
+    <>
+      {open && (
+        <Notification
+          toast
+          time={2000}
+          icon={<Icons.StatusGood color='brand' />}
+          message='Link copied!'
+          onClose={() => setOpen(false)}
+        />
+      )}
+      <Card width='large' background={{ color: 'light-1' }}>
+        <CardHeader direction='column' pad='medium' focusIndicator={false}
+                    onClick={() => navigate(`/blog/diary/${id}`)}>
+          {screenSize !== 'small' && (
+            <Box direction='row' flex='grow' gap='small' alignSelf='end'>
+              {foundDiary.tags.map((tag) => (
+                <Tag
+                  key={tag}
+                  value={tag}
+                  size='small'
+                  // @ts-ignore
+                  style={{ borderWidth: '2px' }}
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    navigate(
+                      createPath({
+                        pathname: routeMap.blogHome,
+                        search: createSearchParams({ tags: [tag] }).toString()
+                      })
+                    )
+                  }}
+                />
+              ))}
+            </Box>
+          )}
+          <Heading level='2' alignSelf='start' margin='none'>
+            {foundDiary.title}
+          </Heading>
+        </CardHeader>
+        <CardBody
+          style={{ paddingTop: 'none', paddingLeft: '24px', paddingBottom: '24px', paddingRight: '24px' }}
+          gap='medium'
+          focusIndicator={false}
+          onClick={() => navigate(routeMap.blogDiaryId(id))}
+        >
+          <Text size='medium' weight='normal' margin='none'>
+            {firstParagraph && firstParagraph.kind === 'paragraph' && firstParagraph.text.slice(0, 240).trim() + '...'}
+          </Text>
+        </CardBody>
+        <CardFooter pad={{ horizontal: 'small' }} background={{ color: 'light-3' }}>
+          <Box direction='row' align='center' justify='between'>
+            <Button
+              icon={isLiked ? <Icons.LikeFill color='brand' /> : <Icons.Like color='text' />}
+              hoverIndicator
+              onClick={likeToggle}
+            />
+            <Box direction='row' gap='small'>
+              <Text size='small' weight='bold' style={{ lineHeight: '20px' }}>
+                ·
+              </Text>
+              <Text size='small'>{readTime} min read</Text>
+            </Box>
           </Box>
-        )}
-        <Heading level='2' alignSelf='start' margin='none'>
-          {foundDiary.title}
-        </Heading>
-      </CardHeader>
-      <CardBody
-        style={{ paddingTop: 'none', paddingLeft: '24px', paddingBottom: '24px', paddingRight: '24px' }}
-        gap='medium'
-        focusIndicator={false}
-        onClick={() => navigate(routeMap.blogDiaryId(id))}
-      >
-        <Text size='medium' weight='normal' margin='none'>
-          {firstParagraph && firstParagraph.kind === 'paragraph' && firstParagraph.text.slice(0, 240).trim() + '...'}
-        </Text>
-      </CardBody>
-      <CardFooter pad={{ horizontal: 'small' }} background={{ color: 'light-3' }}>
-        <Box direction='row' align='center' justify='between'>
-          <Button
-            icon={isLiked ? <Icons.LikeFill color='brand' /> : <Icons.Like color='text' />}
-            hoverIndicator
-            onClick={likeToggle}
-          />
-          <Box direction='row' gap='small'>
-            <Text size='small' weight='bold' style={{ lineHeight: '20px' }}>
-              ·
-            </Text>
-            <Text size='small'>{readTime} min read</Text>
-          </Box>
-        </Box>
-        <Button icon={<Icons.ShareRounded color='text' />} hoverIndicator />
-      </CardFooter>
-    </Card>
+          <Button icon={<Icons.ShareRounded color='text' />} hoverIndicator
+                  onClick={async () => await getUrl(setOpen, foundDiary.id)} />
+        </CardFooter>
+      </Card>
+    </>
   )
 }
