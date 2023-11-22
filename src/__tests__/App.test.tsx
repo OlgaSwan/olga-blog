465675022fe78f62c95751765e789b6b50e2ae1c
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-11-22 23:37:40 +0300

    unit testing with jest

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,26 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
 import Component from '../app/component'
 
-test('demo', () => {
-  expect(true).toBe(true)
+test('Renders the test component', () => {
+  render(<Component />)
+
+  expect(screen.getByTitle('heading')).toHaveTextContent('Component Heading')
+})
+test('main testing', () => {
+  render(<Component />)
+
+  const resetBtn = screen.getByText('Reset')
+  const input = screen.getByPlaceholderText('Enter text')
+  expect(resetBtn).toBeTruthy()
+  expect(resetBtn).not.toBeDisabled()
+  userEvent.click(resetBtn)
+  expect(input).toHaveValue('')
 })
 
-test('Renders the main page', () => {
+test('footer testing', () => {
   render(<Component />)
-  expect(true).toBeTruthy()
+  expect(screen.getByTitle('footering')).toBeTruthy()
+  expect(screen.getByPlaceholderText('Disabled')).toBeDisabled()
 })
