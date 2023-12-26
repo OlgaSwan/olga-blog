5b0d4ca4a2ddc9ef00ed0cfd86a8df3822b6dd1c
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2023-12-26 20:31:15 +0300

    Add 3D animation to diary cards
    import GSAP and adds 3D interactive animation to the diary cards

--- src/model/diary/diary-card.tsx
+++ src/model/diary/diary-card.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent, PropsWithChildren, useContext } from 'react'
+import React, { FunctionComponent, PropsWithChildren, useContext, useRef } from 'react'
 import { createPath, createSearchParams, useNavigate } from 'react-router-dom'
+import { gsap } from 'gsap'
 import { Box, Button, Card, CardBody, CardFooter, CardHeader, Heading, ResponsiveContext, Tag, Text } from 'grommet'
 import * as Icons from 'grommet-icons'
 
@@ -19,12 +20,49 @@ export interface DiaryCardProps {
 export const DiaryCard: FunctionComponent<PropsWithChildren<DiaryCardProps>> = ({ id }) => {
   const allDiaries = useStore(diaryStore.list)
   const foundDiary = allDiaries?.find(p => p.id === id)
+  const cardRef = useRef<HTMLDivElement>(null)
 
   const navigate = useNavigate()
   const screenSize = useContext(ResponsiveContext)
   const readTime = useReadTime(foundDiary?.content)
 
   const [isLiked, likeToggle] = useIsLiked(id)
+
+  const moveCard = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (cardRef.current) {
+      const rect = cardRef.current.getBoundingClientRect()
+      const isInCard =
+        e.clientX >= rect.left && e.clientX <= rect.right && e.clientY >= rect.top && e.clientY <= rect.bottom
+
+      if (isInCard) {
+        const halfWidth = rect.width / 2
+        const halfHeight = rect.height / 2
+        const centerX = rect.left + halfWidth
+        const centerY = rect.top + halfHeight
+
+        const rotateX = ((e.clientY - centerY) / halfHeight) * -10
+        const rotateY = ((e.clientX - centerX) / halfWidth) * 10
+
+        gsap.to(cardRef.current, {
+          rotationY: rotateY,
+          rotationX: rotateX,
+          duration: 0.8,
+          ease: 'power2.out',
+        })
+      }
+    }
+  }
+
+  const resetCard = () => {
+    if (cardRef.current) {
+      gsap.to(cardRef.current, {
+        rotationY: 0,
+        rotationX: 0,
+        duration: 0.8,
+        ease: 'power2.out',
+      })
+    }
+  }
 
   if (!foundDiary)
     return (
@@ -38,8 +76,14 @@ export const DiaryCard: FunctionComponent<PropsWithChildren<DiaryCardProps>> = (
     day: 'numeric',
     month: 'short',
   })
+
   return (
-    <>
+    <div
+      ref={cardRef}
+      onMouseMove={moveCard}
+      onMouseLeave={resetCard}
+      style={{ margin: '0', padding: '0', perspective: '1000px', transformStyle: 'preserve-3d' }}
+    >
       <Card width='large' background={{ color: 'white' }} animation='slideLeft'>
         <CardHeader
           direction='column'
@@ -105,6 +149,6 @@ export const DiaryCard: FunctionComponent<PropsWithChildren<DiaryCardProps>> = (
           <SharedBtn diary_id={foundDiary.id} size='20px' />
         </CardFooter>
       </Card>
-    </>
+    </div>
   )
 }
