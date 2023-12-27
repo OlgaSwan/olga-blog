824edb5a997e968b39472062dc1b493d561eb5b1
parlyment <parlament20000@gmail.com>
2023-12-27 22:12:23 +0200

    move grommet top level container

--- src/shared/loadable-page/placeholder.tsx
+++ src/shared/loadable-page/placeholder.tsx
@@ -2,14 +2,16 @@ import React, { FunctionComponent } from 'react'
 import * as Icons from 'grommet-icons'
 import { Box } from 'grommet'
 
-import { TemplateContent } from '../template'
+import GrommetContainer from '../template/abstract/grommet-container'
 
-export const Placeholder: FunctionComponent = () => (
-  <TemplateContent>
-    <Box flex='grow' direction='column' align='center' justify='center'>
-      <Box animation={{ type: 'pulse', duration: 200, size: 'medium' }}>
-        <Icons.Diamond size='xlarge' color='brand' />
+export const Placeholder: FunctionComponent = () => {
+  return (
+    <GrommetContainer>
+      <Box flex='grow' direction='column' align='center' justify='center'>
+        <Box animation={{ type: 'pulse', duration: 200, size: 'medium' }}>
+          <Icons.Diamond size='xlarge' color='brand' />
+        </Box>
       </Box>
-    </Box>
-  </TemplateContent>
-)
+    </GrommetContainer>
+  )
+}
