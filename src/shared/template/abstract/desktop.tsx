b24870b52d9ac5bed805d415259c1672ebd3c2e6
parlyment <parlament20000@gmail.com>
2023-12-27 22:00:02 +0200

    memo menu & header component

--- src/shared/template/abstract/desktop.tsx
+++ src/shared/template/abstract/desktop.tsx
@@ -1,54 +1,22 @@
 import React, { FunctionComponent } from 'react'
-import { Avatar, Box, Nav, Text } from 'grommet'
+import { Box, Text } from 'grommet'
 import * as Icons from 'grommet-icons'
-import { routeMap } from 'src/shared/route-map'
-import { useMenuData } from 'src/shared/template/abstract/menu/menu-data'
 
 import { LinkCustom } from '../../utils/link-custom'
 import { TemplateProps } from './props'
 
-import avatar from './avatar.jpg'
 import { SwanLogo } from 'src/shared/template/abstract/swan-logo'
 import { MainContent } from 'src/shared/template/abstract/maincontent'
+import Header from './header'
 
 export const TemplateDesktop: FunctionComponent<TemplateProps> = ({ sidebarLeft, main, sidebarRight }) => {
-  const menuData = useMenuData()
   //renders twice when 'main' prop loads cards and animation flashes(( Nujno kak-to peredavat' main v otdel'nyi
   // component no eto impossible
   console.log('render')
 
   return (
     <>
-      <Box
-        as='header'
-        height='xsmall'
-        pad='medium'
-        direction='row'
-        align='center'
-        justify='between'
-        background={{ color: 'background', opacity: 'medium' }}
-        style={{
-          position: 'sticky',
-          top: 0,
-          backdropFilter: 'blur(6px)',
-          zIndex: 1,
-        }}
-      >
-        <Box direction='row' align='center' gap='medium' animation='slideRight'>
-          <LinkCustom
-            size='large'
-            label={<Avatar src={avatar} size='medium' />}
-            href={routeMap.home}
-            data-testid='avatar-link'
-          />
-          <LinkCustom size='large' label='Olga Swan' href={routeMap.home} />
-        </Box>
-        <Nav direction='row' align='center' gap='medium' animation='slideLeft'>
-          {menuData.map((e, index) => (
-            <LinkCustom key={index} style={e.style} label={e.label} href={e.href} onClick={e.onClick} icon={e.icon} />
-          ))}
-        </Nav>
-      </Box>
+      <Header />
       <Box
         direction='row'
         flex='grow'
