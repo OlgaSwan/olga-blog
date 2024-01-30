ff34b4933d6fa7565b3fa96d556bbba899fc1efb
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2024-01-31 01:12:25 +0300

    diaries loading fix

--- src/model/diary/diary-list.tsx
+++ src/model/diary/diary-list.tsx
@@ -2,8 +2,10 @@ import React, { FunctionComponent, useMemo } from 'react'
 import { useStore } from '@nanostores/react'
 import { Box } from 'grommet'
 
-import { diaryStore } from './store'
-import { DiaryCard } from './diary-card'
+import { diaryStore } from 'src/model/diary/store'
+import { DiaryCard } from 'src/model/diary/diary-card'
+
+import * as Icons from 'grommet-icons'
 
 export interface DiaryListProps {
   isSliced?: boolean
@@ -31,6 +33,13 @@ export const DiaryList: FunctionComponent<DiaryListProps> = ({ isSliced = true, 
 
   return (
     <Box gap='medium'>
+      {!allDiaries && (
+        <Box flex='grow' direction='column' align='center' justify='center'>
+          <Box animation={{ type: 'pulse', duration: 200, size: 'medium' }}>
+            <Icons.Diamond size='xlarge' color='brand' />
+          </Box>
+        </Box>
+      )}
       {allDiariesMemo.map(d => (
         <DiaryCard key={d.id} id={d.id} />
       ))}
