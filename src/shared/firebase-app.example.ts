1563b33572ad672a97e781e32af6adbe1d59e306
OlgaSwan <137110816+OlgaSwan@users.noreply.github.com>
2024-06-15 17:13:37 +0300

    Removed firebase-app.ts and added firebase-app.example.ts

---
+++ src/shared/firebase-app.example.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: '<YOUR_API_KEY>',
+  authDomain: '<YOUR_AUTH_DOMAIN>',
+  projectId: '<YOUR_PROJECT_ID>',
+  storageBucket: '<YOUR_STORAGE_BUCKET>',
+  messagingSenderId: '<YOUR_MESSAGING_ID>',
+  appId: '<YOUR_APP_ID>',
+  measurementId: '<YOUR_MEASUREMENT_ID>',
+}
